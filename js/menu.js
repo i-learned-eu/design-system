b83470b6d2dc786758d2a9c00d086813eb731428
Eban Rami <contact+git@eban.eu.org>
2022-03-31 15:14:31 +0300

    [🚀 Fix] Add burger menu animation & fixed positioning

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,12 @@ toggled = true;
 
 function openMenu() {
  if (toggled) {
-   document.querySelector(".right").style.display = "flex";
+   document.querySelector("header .right").style.display = "flex";
+   document.querySelector("header #menuIcon").classList.add("open")
    toggled = !toggled;
  } else {
    document.querySelector(".right").style.display = "none";
+   document.querySelector("header #menuIcon").classList.remove("open")
    toggled = !toggled;
  }
 }

34a1df231be14845b59e3268d92355130eb03009
Eban Rami <contact+git@eban.eu.org>
2022-03-31 15:16:33 +0300

    [🐛 Fix] Cleaner js

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,9 @@ function openMenu() {
  if (toggled) {
    document.querySelector("header .right").style.display = "flex";
    document.querySelector("header #menuIcon").classList.add("open")
-   toggled = !toggled;
  } else {
-   document.querySelector(".right").style.display = "none";
+   document.querySelector("header .right").style.display = "none";
    document.querySelector("header #menuIcon").classList.remove("open")
-   toggled = !toggled;
  }
+ toggled = !toggled;
 }

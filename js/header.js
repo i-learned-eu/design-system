1ac8a6ed8f5878a9e925a7ed9708c9fa6b317c90
Eban Rami <account+gitil@eban.eu.org>
2022-04-17 13:02:47 +0000

    [⚙️ Minor improvement] Remove delay before header change

--- js/header.js
+++ js/header.js
@@ -13,7 +13,6 @@ function openMenu() {
 
 let scrollpos = window.scrollY
 const header = document.querySelector("header")
-const header_height = header.offsetHeight
 
 const add_class_on_scroll = () => header.classList.add("scroll")
 const remove_class_on_scroll = () => header.classList.remove("scroll")
@@ -22,6 +21,6 @@ const remove_class_on_scroll = () => header.classList.remove("scroll")
 window.addEventListener('scroll', function() {
   scrollpos = window.scrollY
 
-  if (scrollpos >= header_height) { add_class_on_scroll() }
+  if (scrollpos > 0) { add_class_on_scroll() }
   else { remove_class_on_scroll() }
 })
